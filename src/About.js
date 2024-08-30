794b5e1cdcf2b87c983a0393ffb3d10a57d3eb9c
MohamadhuAshik <mohamadashik268@gmail.com>
2024-08-30 12:20:43 +0530

    Update Commit

--- src/About.js
+++ src/About.js
@@ -4,7 +4,10 @@ const About = () => {
   return (
     <main className="About">
       <h2>About</h2>
-      <p style={{ marginTop: "1rem" }}>Hi I am Ashik Learn ReactJs</p>
+      <p style={{ marginTop: "1rem" }}>
+        Welcome to my blog! This platform allows you to easily share your thoughts, ideas, and stories. With a simple and user-friendly interface, you can sign up, log in, and start posting your own blogs in no time. You can also update or delete your posts whenever you like. Whether you're here to share your experiences or explore what others have to say, this blog offers a straightforward way to connect with a community of like-minded individuals.
+
+        Join us today and start blogging!</p>
     </main>
   );
 };
