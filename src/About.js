f76aad6d5798942c5038e977166625bcf4421be2
MohamadhuAshik <mohamadashik268@gmail.com>
2024-08-30 12:23:22 +0530

    Update Commit

--- src/About.js
+++ src/About.js
@@ -5,9 +5,9 @@ const About = () => {
     <main className="About">
       <h2>About</h2>
       <p style={{ marginTop: "1rem" }}>
-        Welcome to my blog! This platform allows you to easily share your thoughts, ideas, and stories. With a simple and user-friendly interface, you can sign up, log in, and start posting your own blogs in no time. You can also update or delete your posts whenever you like. Whether you're here to share your experiences or explore what others have to say, this blog offers a straightforward way to connect with a community of like-minded individuals.
-
-        Join us today and start blogging!</p>
+        Welcome to my blog! This platform allows you to easily share your thoughts, ideas, and stories. With a simple and user-friendly interface, you can sign up, log in, and start posting your own blogs in no time. You can also update or delete your posts whenever you like. Whether you're here to share your experiences or explore what others have to say, this blog offers a straightforward way to connect with a community of like-minded individuals.</p>
+      <br></br>
+      <p>Join us today and start blogging!</p>
     </main>
   );
 };
