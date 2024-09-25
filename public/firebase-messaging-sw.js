e1d53911181e9c6088556d9a6b71565ddd74e449
MohamadhuAshik <mohamadashik268@gmail.com>
2024-09-25 16:14:01 +0530

    Notify updated

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -18,11 +18,11 @@ const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
     console.log('Received background message', payload);
-    const notificationTitle = payload.notification.title;
-    const notificationOptions = {
-        body: payload.notification.body,
-        icon: '/logo512.png'
-    };
+    // const notificationTitle = payload.notification.title;
+    // const notificationOptions = {
+    //     body: payload.notification.body,
+    //     icon: '/logo512.png'
+    // };
 
-    self.registration.showNotification(notificationTitle, notificationOptions);
+    // self.registration.showNotification(notificationTitle, notificationOptions);
 });
